bc497f55ce43919c930716f959f38ff7de7f6010
Moussa Hellal <moussa@sane.agency>
2023-06-25 08:11:13 +0100

    Initial Commit

---
+++ CompleteNavigationSwiftUI/CompleteNavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompleteNavigationSwiftUIApp.swift
+//  CompleteNavigationSwiftUI
+//
+//  Created by Moussa on 25/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct CompleteNavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
